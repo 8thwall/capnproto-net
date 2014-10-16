8e944c84c4a0519e634c3109e544db44f60e6da9
Marc Gravell <marc.gravell@gmail.com>
2014-10-16 13:38:05 +0100

    Initial commit

---
+++ CapnProto.net/FieldAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CapnProto
+{
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+    public class FieldAttribute : Attribute
+    {
+        public FieldAttribute(int number)
+        {
+            this.Number= number;
+        }
+        public int Number { get; private set; }
+        public int Offset { get; set; }
+    }
+}
---
+++ CapnProto.net/IdAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace CapnProto
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, AllowMultiple = false, Inherited = false)]
+    [ImmutableObject(true)]
+    public class IdAttribute : Attribute
+    {
+        public IdAttribute(ulong id)
+        {
+            this.Id = id;
+        }
+        public ulong Id { get; private set; }
+    }
+}
