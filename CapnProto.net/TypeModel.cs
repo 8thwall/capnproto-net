2f52018eb00b8546eed2f54e4bccc4dfe3a17e4e
Marc Gravell <marc.gravell@gmail.com>
2014-10-17 06:46:30 +0100

    Tidy up API

--- CapnProto.net/TypeModel.cs
+++ CapnProto.net/TypeModel.cs
@@ -1,43 +1,42 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace CapnProto
 {
-    public class TypeModel
+    public abstract class TypeModel
     {
+        public abstract ITypeSerializer GetSerializer(Type type);
+        public ITypeSerializer<T> GetSerializer<T>()
+        {
+            return (ITypeSerializer<T>)GetSerializer(typeof(T));
+        }
+    }
+
+    public sealed class RuntimeTypeModel : TypeModel
+    {
+        private static readonly RuntimeTypeModel @default = new RuntimeTypeModel();
         public static TypeModel Default { get { return @default; } }
-        private static readonly TypeModel @default = new TypeModel();
 
         readonly Hashtable typeSerializers = new Hashtable();
-        public ITypeSerializer GetSerializer(Type type)
+
+        private ITypeSerializer CreateSerializer(Type type)
+        {
+            if (type == null) throw new ArgumentNullException("type");
+            throw new NotImplementedException();
+        }
+        public override ITypeSerializer GetSerializer(Type type)
         {
             if (type == null) throw new ArgumentNullException("type");
             ITypeSerializer ser = (ITypeSerializer)typeSerializers[type];
-            if(ser == null)
+            if (ser == null)
             {
                 ser = CreateSerializer(type);
-                lock(typeSerializers)
+                lock (typeSerializers)
                 {
                     typeSerializers[type] = ser;
                 }
             }
             return ser;
-        }
-
-        public ITypeSerializer<T> GetSerializer<T>()
-        {
-            return (ITypeSerializer<T>)GetSerializer(typeof(T));
-        }
-
-        internal ITypeSerializer CreateSerializer(Type type)
-        {
-            if (type == null) throw new ArgumentNullException("type");
-
-
-            throw new NotImplementedException();
         }
     }
 }
---
+++ CapnProto.net/TypeSerializer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace CapnProto
+{
+    public static class TypeSerializer
+    {
+        public static object Deserialize(this ITypeSerializer serializer, Stream source)
+        {
+            if (serializer == null) throw new ArgumentNullException("serializer");
+            if (source == null) throw new ArgumentNullException("source");
+            using(var reader = CapnProtoReader.Create(source, null))
+            {
+                return serializer.Deserialize(reader);
+            }
+        }
+        public static T Deserialize<T>(this ITypeSerializer<T> serializer, Stream source)
+        {
+            if (serializer == null) throw new ArgumentNullException("serializer");
+            if (source == null) throw new ArgumentNullException("source");
+            using (var reader = CapnProtoReader.Create(source, null))
+            {
+                return serializer.Deserialize(reader);
+            }
+        }
+    }
+}
